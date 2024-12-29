affee7eccf8355d7f779a0aa76592f4493e2efe6
hikae <account@egahika.dev>
2024-12-30 04:20:17 +0900

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/surf-board/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
